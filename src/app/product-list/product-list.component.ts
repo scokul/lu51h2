8d4d8eb2cdcd85d41a2bdf298afe9298027c16d9
scokul <139473463+scokul@users.noreply.github.com>
2023-07-13 16:39:39 -0500

    5 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,11 +8,17 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
   products = [...products];
 
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify(){
+    window.alert('You will be notified when the product goes on sale')
+  }
+  
 }
 
 
